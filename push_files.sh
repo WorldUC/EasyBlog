09d56c1a2e18fb81bb75f1c5169641b00a2e767c
WorldUC <WorldUC1@yeah.net>
2012-12-11 09:38:19 +0800

    add edit in push_files function

--- push_files.sh
+++ push_files.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
-if [ $# -ne 2 ]
+
+paras_num=$#
+
+if [ $paras_num -ne 2 -a $paras_num -ne 3 ]
 then
-   echo "Usage: $0 SpaceID ItemID"
+   echo "Usage: $0 SpaceID ItemID [type(normal | small | wma)]"
    return 2
 fi
 
+
 mySpaceID=$1
 myItemID=$2
+myType=${3:-"normal"}
+
+if [ $myType != "normal" -a  $myType != "small" -a $myType != "wma" ]
+then
+   echo "Please select collected type"
+   echo "Usage: $0 SpaceID ItemID [type(normal | small | wma)]"
+   return 2
+fi
+   
+
+> newblogs.txt
+
 
 awk 'BEGIN{
       spaceid="'$mySpaceID'";
@@ -21,5 +37,20 @@ awk 'BEGIN{
       close("show.txt");
       system(mycmd2);
       system(mycmd3);
+      system("sed -n \"/<script>openTipWindow../{s,,,; s/.,.*$//; p; q;}\" tmp-post.htm >> newblogs.txt");
       system("sleep 45");
      }' newfiles.txt
+
+clear
+echo "edit blogs..."
+
+if [ $myType = "normal" ]
+then
+    sh edit_blogs.sh $mySpaceID $myItemID
+elif [ $myType = "small" ]
+then 
+    sh edit_blogs_small.sh $mySpaceID $myItemID
+elif [ $myType = "wma" ]
+then
+    sh edit_blogs_wma.sh $mySpaceID $myItemID
+fi
